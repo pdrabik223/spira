68f45e5d7664a9ea55e5c78177789505c1ec273d
Mateusz Kojro <mateuszkojro@outlook.com>
2021-11-25 11:03:06 +0100

    Example of SubProcess class usage

--- spira-example.cpp
+++ spira-example.cpp
@@ -2,8 +2,18 @@
 // Created by piotr on 24/11/2021.
 //
 #include "histogram.h"
-int main(){
+#include "SubProcess.h"
+#include <iostream>
+int main() {
 
   Histogram test("test");
-
+  SubProcess s("root", {"-l", "-e", ".demo"}, "./");
+  std::cout << "=== [ SPIRA ] ===" << std::endl
+            << "Write 'q' to quit" << std::endl;
+  char x;
+  do {
+    std::cout << ">>> ";
+    std::cin >> x;
+    std::cout << std::endl;
+  } while (x != 'q');
 }
