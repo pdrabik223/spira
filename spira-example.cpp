07f2ae0c3496a9f4c6d53ef4866b1a747003c8e3
piotr233 <piotr.drabik@edu.uni.lodz.pl>
2021-11-26 20:16:29 +0100

    updated example file

--- spira-example.cpp
+++ spira-example.cpp
@@ -1,35 +1,19 @@
 //
 // Created by piotr on 24/11/2021.
 //
-#include "histogram.h"
 #include "SubProcess.h"
+#include "histogram.h"
 #include <iostream>
 int main() {
-  try {
+  //  try {
+  //
+  Histogram test("test");
+  test.min_bin_ = 0;
+  test.max_bin_ = 100;
+  test.bin_count_ = 100;
 
-    Histogram test("test");
-    test.min_bin_ = 0;
-    test.max_bin_ = 100*100;
-    test.bin_count_ = 100;
-
-    for(int i =0;i<100;i++){
-      test.Fill(i*i);
-    }
-
-    test.GenFile();
-
-    SubProcess s("root", {"-l", "-X", "../test.cpp"}, "./");
-    std::cout << "=== [ SPIRA ] ===" << std::endl
-              << "Write 'q' to quit" << std::endl;
-    char x;
-    do {
-      std::cout << ">>> ";
-      std::cin >> x;
-      std::cout << std::endl;
-    } while (x != 'q');
-
-  } catch (const CustomError &e) {
-    std::cout << "Runtime error: " << e.what() << std::endl;
-    return 1;
+  for (int i = 0; i < 100; i++) {
+    test.Fill(i);
   }
+  test.DrawPlot();
 }
